888630c9287345e6b6a7e0e2d103f34b8fb2ac19
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2021-11-14 04:53:29 -0800

    Create tearsheet.php

---
+++ tearsheet/tearsheet.php
@@ -0,0 +1,195 @@
+<!DOCTYPE HTML>
+<!--
+	Multiverse by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Tearsheet Portfolio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+        <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../assets/css/fontawesome.css">
+    <link rel="stylesheet" href="../assets/css/templatemo-stand-blog.css">
+    <link rel="stylesheet" href="../assets/css/owl.css">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+    <header class="">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="../index.php"><h2>Gordwin Odhiambo</h2></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="">
+              <a class="nav-link" href="../index.php">Home
+                
+              </a>
+            </li> 
+            <li class="nav-item">
+              <a class="nav-link" href="../about.php">Bio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../updates.php">OnPublications</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="../tearsheet/tearsheet.php">Tearsheets
+              <span class="sr-only">(current)</span>
+            </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#post-details.html">Videos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../contact.php">PrintSale</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="../index.php"><strong>Home</strong></a></h1>
+						<nav>
+							<ul>
+								<li><a href="#footer" class="icon fa-info-circle">Info</a></li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+						<article class="thumb">
+							<a href="images/fulls/tearsheet1.png" class="image"><img src="images/thumbs/tearsheet1.png" alt="" /></a>
+							<h2>For La Monde</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet2.jpg" class="image"><img src="images/thumbs/tearsheet2.jpg" alt="" /></a>
+							<h2>DW Akademie</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet8.jpg" class="image"><img src="images/thumbs/tearsheet8.jpg" alt="" /></a>
+							<h2>Open Society Foundation</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet3.jpg" class="image"><img src="images/thumbs/tearsheet3.jpg" alt="" /></a>
+							<h2>NPR</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet4.jpg" class="image"><img src="images/thumbs/tearsheet4.jpg" alt="" /></a>
+							<h2>De Spiegel</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet7.jpg" class="image"><img src="images/thumbs/tearsheet7.jpg" alt="" /></a>
+							<h2>La Monde</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet6.jpg" class="image"><img src="images/thumbs/tearsheet6.jpg" alt="" /></a>
+							<h2>L Express</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet5.jpg" class="image"><img src="images/thumbs/tearsheet5.jpg" alt="" /></a>
+							<h2>The Gurdian</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet9.jpg" class="image"><img src="images/thumbs/tearsheet9.jpg" alt="" /></a>
+							<h2>Magnum Foundation</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet10.jpg" class="image"><img src="images/thumbs/tearsheet10.jpg" alt="" /></a>
+							<h2>EPIC PEOPLE</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet11.jpg" class="image"><img src="images/thumbs/tearsheet11.jpg" alt="" /></a>
+							<h2>World Press Photo Exhibition - 2020</h2>
+							<p>2021.</p>
+						</article>
+						<article class="thumb">
+                        <a href="images/fulls/tearsheet12.jpg" class="image"><img src="images/thumbs/tearsheet12.jpg" alt="" /></a>
+							<h2>Visura</h2>
+							<p>2021.</p>
+						</article>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer" class="panel">
+						<div class="inner split">
+							<div>
+								<section>
+									<h2>Notice</h2>
+									<p>The owner of this site; Gordwin Odhiambo, May not be fully responsible for external contents mentioned on this tearsheet .</p>
+								</section>
+								<section>
+									<h2>Follow me on ...</h2>
+									<ul class="icons">
+										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										
+										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										
+										
+										<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									</ul>
+								</section>
+								<p class="copyright">
+									&copy; home: <a href="../index.php"></a>.
+								</p>
+							</div>
+							<div>
+								<section>
+									<h2>Get in touch</h2>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<input type="text" name="name" id="name" placeholder="Name" />
+											</div>
+											<div class="field half">
+												<input type="text" name="email" id="email" placeholder="Email" />
+											</div>
+											<div class="field">
+												<textarea name="message" id="message" rows="4" placeholder="Message"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send" class="primary" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</form>
+								</section>
+							</div>
+						</div>
+					</footer>
+
+			</div>
+            <?php include '../includes/footer.php';?>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
