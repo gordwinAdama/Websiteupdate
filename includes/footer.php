ababb682a2eb2d4bc09d18d7a2f08230d042b005
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2021-11-14 05:10:10 -0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="social-icons">
+              
+              <li><a href="https://www.instagram.com/gordwin_docu_photo/" target = "blank"><i class="fa fa-instagram"></i></a></li>
+              
+              <li><a href="https://www.linkedin.com/in/gordwin-odhiambo-8b441bb8/" target = "blank"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-lg-12">
+            <div class="copyright-text">
+              <p>Copyright 2021 - No use use of any of this site content without permision!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
