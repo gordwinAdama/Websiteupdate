af2fd9cc2b16ba250c9a7ecae0dff8f905c9bfb5
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2021-11-14 05:09:43 -0800

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<header class="">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><h2>Gordwin Odhiambo</h2></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li>
+              <a class="nav-link" href="index.php">Home
+                
+              </a>
+            </li> 
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">Bio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="updates.php">OnPublications</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./tearsheet/tearsheet.php">Tearsheets</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#post-details.html">Videos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="prints.php">PrintSale</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
